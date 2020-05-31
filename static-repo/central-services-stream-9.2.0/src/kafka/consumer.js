ffb6cb2a43e15efbbfc13ae3a1dac6375cd071bf
Joran Dirk Greef <joran@ronomon.com>
2020-05-31 15:52:34 +0200

    Set CONCURRENCY=10, BATCH_COUNT=10, BATCH_TIMEOUT=10

--- static-repo/central-services-stream-9.2.0/src/kafka/consumer.js
+++ static-repo/central-services-stream-9.2.0/src/kafka/consumer.js
@@ -38,9 +38,9 @@
 'use strict'
 
 // coil-perf:
-const CONCURRENCY = 1
-const KAFKA_BATCH_COUNT = 7
-const KAFKA_BATCH_TIMEOUT = 13000
+const CONCURRENCY = 10
+const KAFKA_BATCH_COUNT = 10
+const KAFKA_BATCH_TIMEOUT = 10
 
 const EventEmitter = require('events')
 const async = require('async')
